e62f21ca69e6b9cd8bc9d5a97d00ce3c0c7e12d1
thiliporace <85422792+thiliporace@users.noreply.github.com>
2023-05-09 21:00:29 +0000

    Initial commit

---
+++ EnemyAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour, IDamageable
+{
+    public AttackRadius AttackRadius;
+    public EnemyMovement Movement;
+    public NavMeshAgent Agent;
+    public Target target;
+
+    private Coroutine LookCoroutine;
+    private const string ATTACK_TRIGGER = "Attack";
+
+    private void Awake()
+    {
+        AttackRadius.OnAttack += OnAttack;
+    }
+
+    private void OnAttack(IDamageable victim)
+    {
+
+        if (LookCoroutine != null)
+        {
+            StopCoroutine(LookCoroutine);
+        }
+
+        LookCoroutine = StartCoroutine(LookAt(victim.GetTransform()));
+    }
+
+    private IEnumerator LookAt(Transform Target)
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(Target.position - transform.position);
+        float time = 0;
+
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, time);
+
+            time += Time.deltaTime * 2;
+            yield return null;
+        }
+
+        transform.rotation = lookRotation;
+    }
+    
+    public void TakeDamage(int Damage)
+    {
+        target.TakeDamage(Damage);
+
+        if (target.health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+}
